2e582b3ecb5fafe40266b570b8d78a305302b9fd
Rar Black <rar18ack@gmail.com>
2025-06-05 14:41:03 +0100

    initial commit

---
+++ get_categories.py
@@ -0,0 +1,111 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
+import time
+
+BASE_URL = "https://www.waitrose.com"
+BASE_CATEGORIES = {
+    "fresh_and_chilled": "/ecom/shop/browse/groceries/fresh_and_chilled",
+    "bakery": "/ecom/shop/browse/groceries/bakery",
+    "beer_wine_and_spirits": "/ecom/shop/browse/groceries/beer_wine_and_spirits",
+    "frozen": "/ecom/shop/browse/groceries/frozen",
+    "summer": "/ecom/shop/browse/groceries/summer",
+    "offers": "/ecom/shop/browse/offers",
+}
+
+def crawl(search_term):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    url = f'{BASE_URL}/{BASE_CATEGORIES["bakery"]}'
+    driver.get(url)
+
+    reject_all_button = driver.find_element(By.XPATH, "//button[@data-webviewid='reject-cookies']")
+    reject_all_button.send_keys(Keys.RETURN)
+
+    time.sleep(10)
+
+    # assert "Load more" in driver.page_source
+
+    # while (True):
+    #     try:
+    #         load_more_button = driver.find_element(By.XPATH, "//button[@aria-label='Load more']")
+    #         load_more_button.send_keys(Keys.RETURN)
+    #         time.sleep(2)
+
+    #     except Exception as e:
+    #         break
+
+    # time.sleep(2)
+    
+    # results = driver.find_elements(By.XPATH, './/article[@data-testid="product-pod"]')
+
+    # products = []
+    # for results in results:
+    #     try:
+    #         product_badge = product.find_element(By.XPATH, './/span[@data-testid="product-badge"]/span').text
+    #     except NoSuchElementException:
+    #         product_badge = ""
+    #     except Exception as e:
+    #         print(e)
+
+    #     try:
+    #         name = product.find_element(By.XPATH, './/h2[@data-testid="product-pod-name"]/span').text
+    #     except NoSuchElementException:
+    #         name = ""
+    #     except Exception as e:
+    #         print(e)
+
+    #     try:
+    #         price = product.find_element(By.XPATH, './/span[@data-test="product-pod-price"]/span').text
+    #     except NoSuchElementException:
+    #         price = ""
+    #     except Exception as e:
+    #         print(e)
+
+    #     try:
+    #         product_rating = product.find_element(By.XPATH, './/a[@aria-label="Product Rating"]/div/span').text
+    #     except NoSuchElementException:
+    #         product_rating = ""
+    #     except Exception as e:
+    #         print(e)
+
+    #     try:
+    #         offer_description = product.find_element(By.XPATH, './/span[@data-testid="offer-description"]/span').text
+    #     except NoSuchElementException:
+    #         offer_description = ""
+    #     except Exception as e:
+    #         print(e)
+
+    #     try:
+    #         was_price_description = product.find_element(By.XPATH, './/em[@data-testid="was-price-description"]').text
+    #     except NoSuchElementException:
+    #         was_price_description = price
+    #     except Exception as e:
+    #         print(e)
+
+    #     products.append(
+    #         {
+    #             "product_badge": product_badge,
+    #             'name': name, 
+    #             'price': price, 
+    #             "product_rating": product_rating,
+    #             'offer_description': offer_description, 
+    #             'was_price_description': was_price_description, 
+    #         }
+    #     )
+
+    driver.quit()
+
+    return products
+
+
+if __name__ == "__main__":
+    products = crawl("coffee")
+
+    for product in products:
+        print(product)
+
