3b1748c29c680d4e66a123455c9f4fbeed9cff95
Rar Black <rar18ack@gmail.com>
2025-08-02 22:41:26 +0100

    update new changes

--- products/network-scraping/get_products.py
+++ products/network-scraping/get_products.py
@@ -1,13 +1,19 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from seleniumwire import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from queue import Queue
 import threading
 import pandas as pd
 from pathlib import Path
 import pickle, json, time
 from enum import Enum
+from urllib3.exceptions import ProtocolError
+import logging
+import datetime
+
+
+logger = logging.getLogger(__name__)
 
 type Page = dict[str, str | list]
 
@@ -19,12 +25,12 @@ BASE_URL = "https://www.waitrose.com"
 SEARCH_PATH = "ecom/shop/browse"
 
 INITIAL_CATEGORIES: dict[str, str] = {
-    'groceries/summer': 'SUMMER',
-    'groceries/fresh_and_chilled': 'Fresh & Chilled',
-    'groceries/frozen': 'Frozen',
+    # 'groceries/summer': 'SUMMER',
+    # 'groceries/fresh_and_chilled': 'Fresh & Chilled',
+    # 'groceries/frozen': 'Frozen',
     'groceries/beer_wine_and_spirits':'Beer, Wine & Spirits',
-    'groceries/bakery': 'Bakery',
-    'offers': 'Offers'
+    # 'groceries/bakery': 'Bakery',
+    # 'offers': 'Offers'
 }
 
 BASE_PATH = "/Users/rarblack/.dev/organizations/rarblack/waitrose-product-scraper"
@@ -33,12 +39,12 @@ CATEGORIES_PATH = "/Users/rarblack/.dev/organizations/rarblack/waitrose-product-
 CATEGORIES:pd.DataFrame = None
 
 CACHE_PATH = "/Users/rarblack/.dev/organizations/rarblack/waitrose-product-scraper/products/cache.pkl"
-CACHE: dict[str, bool] = {}
+CACHE: dict[str, int | None] = None
 
 TASK_CACHE_PATH = "/Users/rarblack/.dev/organizations/rarblack/waitrose-product-scraper/products/task_cache.pkl"
-TASK_CACHE: list[str] = []
+TASK_CACHE: list[str] = None
 
-THRESHOLD = 10
+THRESHOLD = 1
 
 TASK_QUEUE = Queue()
 DRIVER = webdriver.Chrome(seleniumwire_options={'disable_encoding': True})
@@ -105,47 +111,51 @@ def clean_data(data: dict) -> list[dict]:
 
     return items
 
-def sort_a2z():
-    try:
-        DRIVER.find_element(By.XPATH, "//div[@id='product-refinements']/div[2]/section[2]/div[1]/button").click()
-        DRIVER.find_element(By.XPATH, "//input[@name='A_2_Z']").click()
-        time.sleep(1)
-    except NoSuchElementException:
-        DRIVER.find_element(By.XPATH, "//div[@id='product-refinements']/div[2]/section[1]/div[1]/button").click()
-        DRIVER.find_element(By.XPATH, "//input[@name='A_2_Z']").click()
-        time.sleep(1)
-    
-def load_all_data():
-    while (True):
+def crawl(url: str) -> tuple[str, list[dict]]:
+    def sort_a2z():
         try:
-            load_more_button = DRIVER.find_element(By.XPATH, "//button[@aria-label='Load more']")
-            load_more_button.send_keys(Keys.RETURN)
-            time.sleep(.5)
+            DRIVER.find_element(By.XPATH, "//div[@id='product-refinements']/div[2]/section[2]/div[1]/button").click()
+            DRIVER.find_element(By.XPATH, "//input[@name='A_2_Z']").click()
+            time.sleep(1)
+        except NoSuchElementException:
+            DRIVER.find_element(By.XPATH, "//div[@id='product-refinements']/div[2]/section[1]/div[1]/button").click()
+            DRIVER.find_element(By.XPATH, "//input[@name='A_2_Z']").click()
+            time.sleep(1)
 
-        except Exception as e:
-            break
+    def load_data() -> int:
+        cnt: int = 0
+        while (True):
+            try:
+                load_more_button = DRIVER.find_element(By.XPATH, "//button[@aria-label='Load more']").click()
+                # load_more_button.send_keys(Keys.RETURN)
 
-    time.sleep(.5)
+                cnt += 1
 
-def crawl() -> tuple[str, list[dict]]:
+                print(f'    Loading {cnt}', end='\r')
+
+                time.sleep(.5)
+
+            except NoSuchElementException as error:
+                # log error as info
+                print(f'    Loading {cnt}')
+                return cnt
+            
     def crawl_category() -> list[dict[str, str]]: 
-        category = None
+        category: str = None
         for request in DRIVER.requests:
             if request.response and request.method == 'POST':
                 if request.url.endswith("live?clientType=WEB_APP&tag=browse"):
                     try:
                         body = json.loads(request.body.decode('utf-8'))
                         category = body['variables']['category']
-
                         break
+
                     except Exception as error:
                         print(error)
 
         return category
     
-    def crawl_data() -> list[dict[str, str]]: 
-        load_all_data()
-        
+    def crawl_data() -> list[dict[str, str]]:         
         data = []
         for request in DRIVER.requests:
             if request.response and request.method == 'POST':
@@ -160,7 +170,7 @@ def crawl() -> tuple[str, list[dict]]:
 
         return data
 
-    def crawl_tasks():
+    def crawl_tasks() -> list[dict[str, str]]:
         elements = DRIVER.find_elements(By.XPATH, './/ul[@data-testid="category-list-links"]/li')
 
         categories = []
@@ -174,23 +184,34 @@ def crawl() -> tuple[str, list[dict]]:
                 category = url.split('/')[-1]
                 badge = text
 
-                categories.append({"url": url, "category": category, "badge": badge})
+                categories.append({"url": url, "path": category, "badge": badge})
 
             except NoSuchElementException:
                 pass # add logger
-            except Exception as e:
-                print(e)
+            except Exception as error:
+                print(error)
 
         return categories
 
+    DRIVER.get(url)
+
+    sort_a2z()
+
+    data_bulk_count: int = load_data()
+    CACHE[url] = data_bulk_count
+    
     category = crawl_category()
+    print(category)
+
     data = crawl_data()
 
     tasks = crawl_tasks()
     for task in tasks:
-        TASK_QUEUE.put(task['url'])
+        if not CACHE.get(task['url'], False):
+            TASK_QUEUE.put(task['url'])
 
     return {
+        "url": url,
         "category": category, 
         "tasks": tasks, 
         "data": data
@@ -202,67 +223,74 @@ def worker() -> None:
         url_raw = TASK_QUEUE.get()
 
         url = clean_url(url_raw)
+        logger.info(f"TASK: {url}")
+        print(f"TASK: {url}")
 
         try:
-            DRIVER.get(url)
-
-            sort_a2z()
-            
-            result = crawl()
-            result['url'] = url
-
-            # print(result)
-            # write_json(result, path=f'{BASE_PATH}/products/network-scraping/data/{time.time()}.json')
-            # break
-
+            result = crawl(url)
             results.append(result)
 
-            CACHE[url] = True
             if len(results) >= THRESHOLD:
                 timestamp = time.time()
+
                 write_json(results, path=f'{BASE_PATH}/products/network-scraping/data/{timestamp}.json')
                 
                 results.clear()
 
                 write_cache()
-
-        except NoSuchElementException as error:
-            print(error)
-            print(url)
-
+            
         except Exception as error: # TODO: separate exception to handle errors better
-            print(error)
+            logger.error(error)
+            print(CACHE)
             print(url)
-            CACHE[url] = False
+
+            CACHE[url] = None
             TASK_QUEUE.put(url)
 
             while not TASK_QUEUE.empty():
                 TASK_CACHE.append(TASK_QUEUE.get(False))
-            write_pickle(TASK_CACHE, TASK_CACHE_PATH)
+
             write_cache(CACHE, CACHE_PATH)
+            write_pickle(TASK_CACHE, TASK_CACHE_PATH)
         
         TASK_QUEUE.task_done()
 
-if __name__ == "__main__":
-    # cache file
-    if Path(CACHE_PATH).exists():
-        CACHE = read_pickle(CACHE_PATH)
+def load_cache() -> None:
+    if Path(CACHE_PATH).exists(): # cache file
+        logger.info("Cache read")
+        return read_pickle(CACHE_PATH)
+    
+    return {}
 
-    # tasks file
-    if Path(TASK_CACHE_PATH).exists():
-        TASK_CACHE = read_pickle(TASK_CACHE_PATH)
+def load_tasks() -> None:
+    if Path(TASK_CACHE_PATH).exists(): # tasks file
+        logger.info("Task cache read")
+        return read_pickle(TASK_CACHE_PATH)
+
+    return []
+
+def main() -> None:
+    today = datetime.date.today()
+    logging.basicConfig(filename=f'{BASE_PATH}/products/network-scraping/logs/{today}.log', level=logging.INFO)
+
+    CACHE = load_cache()
+    TASK_CACHE = load_tasks()
 
     if TASK_CACHE:
+        logging.info(f'Starting from cache')
+        print(f'Starting from cache')
+
         for url in TASK_CACHE:
-            TASK_QUEUE.put(url)
+            if not CACHE.get(url, False):
+                TASK_QUEUE.put(url)
     else:
+        logging.info(f'Starting from scratch')
+        print(f'Starting from scratch')
         for category, badge in INITIAL_CATEGORIES.items():
             url = f'{BASE_URL}/{SEARCH_PATH}/{category}'
 
             if not CACHE.get(url, False):
                 TASK_QUEUE.put(url)
-
-    # TASK_QUEUE.put("https://www.waitrose.com/ecom/shop/browse/offers")
 
     if not TASK_QUEUE.empty():
         DRIVER.get(f'{BASE_URL}')
@@ -271,4 +299,15 @@ if __name__ == "__main__":
         threading.Thread(target=worker, daemon=True).start()
         TASK_QUEUE.join()
 
-    DRIVER.quit()
+
+if __name__ == "__main__":
+    try:
+        main()
+        DRIVER.quit()
+
+    except Exception as error:
+        DRIVER.quit()    
+
+    
+
+
